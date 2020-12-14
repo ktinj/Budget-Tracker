af713b7a86378a55b4cac0844af99e68909d623d
Kristin Jacobsen <kristinjacobsenn@gmail.com>
2020-12-14 14:34:40 -0600

    deploying

--- Develop/server.js
+++ Develop/server.js
@@ -15,13 +15,17 @@ app.use(express.json());
 
 app.use(express.static("public"));
 
-mongoose.connect("mongodb://localhost/budget", {
+mongoose.connect(
+  process.env.MONGODB_URI || "mongodb://localhost/budget", {
   useNewUrlParser: true,
+  useUnifiedTopology: true,
+  useCreateIndex: true,
   useFindAndModify: false
 });
 
 // routes
 app.use(require("./routes/api.js"));
+app.use(require("./routes/htmlRoutes"));
 
 app.listen(PORT, () => {
   console.log(`App running on port ${PORT}!`);
